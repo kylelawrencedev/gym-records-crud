9bc98a113261c16e9c7c234b6d2413d9194095e1
kylelawrencedev <kylelawrence19@gmail.com>
2021-07-17 11:15:04 +0000

    Add 404 functionality

--- app.py
+++ app.py
@@ -206,7 +206,16 @@ def delete_workout(exercise_id):
     return redirect(url_for("get_overview"))
 
 
+# Error Handling
+@app.errorhandler(404)
+def page_not_found(e):
+
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
