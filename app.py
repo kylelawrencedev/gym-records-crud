cc62063aa24a0b14ba2fdbbe3c7bf4d69c63097d
kylelawrencedev <kylelawrence19@gmail.com>
2021-07-21 17:11:52 +0000

    Change debugger to false

--- app.py
+++ app.py
@@ -398,4 +398,4 @@ Renders Template : 500.html
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
